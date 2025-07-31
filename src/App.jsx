8c4ab299c99553a4d74c24ec4caba5fb08899e42
joonlee <96dalee@gmail.com>
2025-07-31 11:05:54 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+// App.jsx
+import Counter from "./components/Counter.jsx";
+
 const App = () => {
-  return <div className="bg-red-100 min-h-screen flex justify-center items-center">
-    App
-</div>;
+  return (
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      <Counter />
+    </div>
+  );
 };
 
 export default App;
